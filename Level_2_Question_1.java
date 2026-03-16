808d6e799d9fd783489eef8d6d91894e9bcc130f
Deb Jyoti Ghosh <debjyoti08adamas@gmail.com>
2026-03-16 13:18:27 +0530

    Changing file name

--- Level_2_Question_1.java
+++ Level_2_Question_1.java
@@ -1,6 +1,6 @@
  import java.util.Scanner;
 
-public class OddEvenNumbers {
+public class Level_2_Question_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
